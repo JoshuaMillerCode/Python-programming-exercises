d807e56e2aff9e2a5ba6119119a172c0f4fbb219
Joshua <millerjoshua737@gmail.com>
2022-04-13 23:25:23 -0500

    Q6 in progress

--- solutions/level-1/6-question.py
+++ solutions/level-1/6-question.py
@@ -1,1 +1,21 @@
 # Solution
+import math
+# Formula: Q = Square root of [(2 * C * D)/H]
+
+# work in progess.
+
+
+def weird_formula():
+    D = input("Enter number: ")
+    C = 60
+    H = 30
+    nums = D.split(",")
+    ls = []
+    for x in nums:
+        calc = 2 * C * float(x) / H
+        sqrt = int(math.sqrt(calc))
+        ls.append(str(sqrt))
+    print(", ".join(ls))
+
+
+weird_formula()
