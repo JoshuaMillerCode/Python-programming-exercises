d272af85beca9f3945b7a22181d94b5483167afa
Joshua <millerjoshua737@gmail.com>
2022-04-16 20:31:03 -0500

    Q10 solved

--- solutions/level-1/10-question.py
+++ solutions/level-1/10-question.py
@@ -1,1 +1,13 @@
 # My Solution
+# akjsdhfjlkashdfkjasdhfkjashfdkjhaskjfhjkashfkjahsdfjkhaskdjfhakjshfjkasdhfkjhaskjfhasjkdfh
+words = [x for x in input().split(" ")]
+
+tracker = {}
+
+for word in words:
+    if word in tracker:
+        pass
+    else:
+        tracker[word] = 1
+
+print(" ".join(sorted([*tracker])))
